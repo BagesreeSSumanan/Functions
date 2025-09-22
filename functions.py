1174387195b210550298dd0ee4c0040f5bfd443e
Bagesree S Sumanan <bagesree.s.sumanan@xminds.com>
2025-09-22 15:17:40 +0530

    added loop

--- functions.py
+++ functions.py
@@ -13,10 +13,9 @@ def powerOfnumber(num, power):
         return num * powerOfnumber(num, power - 1) 
 
 def student_data(**kwargs):
-    print("Student ID:", kwargs.get("student_id"))
-    print("Student Name:", kwargs.get("student_name"))
-    print("Student Class:", kwargs.get("student_class"))
-    print("Student Average Mark:", kwargs.get("student_avgMark"))
+    for i in kwargs:
+        print(i,":", kwargs.get(i))
+   
 factorial(5)
 print("3 ^ 2 is", powerOfnumber(3, 2))
 student_data(student_id=1,student_name="Hari",student_class ="10th",student_avgMark="98")
